000c867f5d87ac716d8f99e74f8686e28bcb6ee3
Martin <martinzx6r@hotmail.com>
2015-07-13 02:34:09 +0200

    Autoload de la BD

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,30 @@
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si la ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+	models.Quiz.find(quizId).then(
+		function(quiz) {
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			}else { 
+				next(new Error('No existe quizID = ' + quizId));
+			}
+		}
+		).catch(function(error){next(error);});
+};
+
 // GET /quizes
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(function(quizes){
 		res.render('quizes/index',{quizes: quizes});
-	})
+	}).catch(function(error) { next(error);})
 };
 
 //GET /quizes/:id
 exports.show = function(req, res) {
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show',{quiz: quiz});
+		res.render('quizes/show',{quiz: req.quiz});
 	})
 };
 
@@ -18,15 +32,10 @@ exports.show = function(req, res) {
 //GET /quizes/answer
 
 exports.answer = function(req, res){
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if (req.query.respuesta === quiz.respuesta)
-		{
-			res.render('quizes/answer',
-			{quiz: quiz, respuesta: 'Correcto'});
-		}else{
-			res.render('quizes/answer',
-			{quiz: quiz, respuesta: 'Incorrecto'});
-		
-		}
-	})
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta)
+	{
+		resultado = 'Correcto';
+	}
+	res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
 };
