00b3df90adb0df8c298a5730113dc01c1eb81868
Martin <martinzx6r@hotmail.com>
2015-07-14 00:49:32 +0200

    Añadimos busqueda de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,9 +16,24 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index',{quizes: quizes});
-	}).catch(function(error) { next(error);})
+	var busqueda  = "%";
+	if (req.query.search != undefined)
+	{
+		busqueda = "%" + req.query.search + "%";
+		busqueda = busqueda.trim();
+		busqueda = busqueda.replace(/\s+|\s+$/g,"%");
+
+	}
+	
+	models.Quiz.findAll({where:["pregunta like ?", busqueda]}).then(function(quizes){
+				res.render('quizes/index',{quizes: quizes});
+			}).catch(function(error) { next(error);})
+	
+	
+	//models.Quiz.findAll().then(function(quizes){
+	//	res.render('quizes/index',{quizes: quizes});
+	//		}).catch(function(error) { next(error);})
+	
 };
 
 //GET /quizes/:id
